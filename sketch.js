ff2dcd5ff0a5c60bda352b741aed0e739a84aa6f
Ariel Guerrero <ariel.guerrero@ucuenca.edu.ec>
2024-01-27 18:25:29 -0500

    Proyecto final - corregido

--- sketch.js
+++ sketch.js
@@ -3,12 +3,12 @@ const celdas = []; //4x4
 //LOS ARRAY SIMPRE COMO CONST-CONTANTES
 const RETICULA = 8;
 //tanto el numero de azulejos y reticulas van en mayuscula al ser numeros que no van a varias, en minusculas son array que si cambian
-const azulejos = [];
-const NA = 14; //numero de azulejos
-let opcionesI = [];
-
 let ancho;
 let alto;
+
+const azulejos = [];
+
+let opcionesI = [];
 
 const reglas = [
   //reglas de los bordes de cada azulejo
@@ -68,41 +68,41 @@ const reglas = [
     DOWN: 0,
     LEFT: 0,
   },
-  // {
-  //     //tiles8
-  //     UP: 0,
-  //     RIGHT: 5,
-  //     DOWN: 0,
-  //     LEFT: 0,
-  //   },
-  //   {
-  //     //tiles9
-  //     UP: 1,
-  //     RIGHT: 2,
-  //     DOWN: 0,
-  //     LEFT: 5,
-  //   },
-  //   {
-  //     //tiles10
-  //     UP: 0,
-  //     RIGHT: 2,
-  //     DOWN: 0,
-  //     LEFT: 2,
-  //   },
-  //   {
-  //     //tiles11
-  //     UP: 0,
-  //     RIGHT: 0,
-  //     DOWN: 0,
-  //     LEFT: 2,
-  //   },
-  //   {
-  //     //tiles12
-  //     UP: 3,
-  //     RIGHT: 0,
-  //     DOWN: 3,
-  //     LEFT: 0,
-  //   },
+  {
+    //tiles8
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tiles9
+    UP: 1,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles10
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles11
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles12
+    UP: 4,
+    RIGHT: 0,
+    DOWN: 4,
+    LEFT: 0,
+  },
   {
     //tiles13
     UP: 0,
@@ -117,41 +117,41 @@ const reglas = [
     DOWN: 1,
     LEFT: 0,
   },
-  //   {
-  //     //tiles15
-  //     UP: 0,
-  //     RIGHT: 0,
-  //     DOWN: 3,
-  //     LEFT: 0,
-  //   },
-  //   {
-  //     //tiles16
-  //     UP: 3,
-  //     RIGHT: 5,
-  //     DOWN: 0,
-  //     LEFT: 0,
-  //   },
-  //   {
-  //     //tiles17
-  //     UP: 0,
-  //     RIGHT: 5,
-  //     DOWN: 0,
-  //     LEFT: 5,
-  //   },
-  //   {
-  //     //tiles18
-  //     UP: 1,
-  //     RIGHT: 2,
-  //     DOWN: 1,
-  //     LEFT: 5,
-  //   },
-  //   {
-  //     //tiles19
-  //     UP: 3,
-  //     RIGHT: 0,
-  //     DOWN: 0,
-  //     LEFT: 2,
-  //   },
+  {
+    //tiles15
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 4,
+    LEFT: 0,
+  },
+  {
+    //tiles16
+    UP: 4,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tiles17
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles18
+    UP: 1,
+    RIGHT: 2,
+    DOWN: 1,
+    LEFT: 2,
+  },
+  {
+    //tiles19
+    UP: 4,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 2,
+  },
   {
     //tiles620
     UP: 0,
@@ -166,20 +166,20 @@ const reglas = [
     DOWN: 1,
     LEFT: 0,
   },
-  //   {
-  //     //tiles22
-  //     UP: 1,
-  //     RIGHT: 0,
-  //     DOWN: 4,
-  //     LEFT: 0,
-  //   },
-  //   {
-  //     //tiles23
-  //     UP: 0,
-  //     RIGHT: 0,
-  //     DOWN: 3,
-  //     LEFT: 0,
-  //   },
+  {
+    //tiles22
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 3,
+    LEFT: 0,
+  },
+  {
+    //tiles23
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 4,
+    LEFT: 0,
+  },
   {
     //tiles24
     UP: 1,
@@ -187,48 +187,48 @@ const reglas = [
     DOWN: 0,
     LEFT: 0,
   },
-  //   {
-  //     //tiles25
-  //     UP: 0,
-  //     RIGHT: 5,
-  //     DOWN: 0,
-  //     LEFT: 5,
-  //   },
-  //   {
-  //     //tiles26
-  //     UP: 4,
-  //     RIGHT: 0,
-  //     DOWN: 4,
-  //     LEFT: 0,
-  //   },
-  //   {
-  //     //tiles27
-  //     UP: 0,
-  //     RIGHT: 2,
-  //     DOWN: 0,
-  //     LEFT: 2,
-  //   },
-  //   {
-  //     //tiles28
-  //     UP: 0,
-  //     RIGHT: 0,
-  //     DOWN: 0,
-  //     LEFT: 2,
-  //   },
-  //   {
-  //     //tiles29
-  //     UP: 0,
-  //     RIGHT: 5,
-  //     DOWN: 0,
-  //     LEFT: 0,
-  //   },
-  //   {
-  //     //tiles30
-  //     UP: 4,
-  //     RIGHT: 0,
-  //     DOWN: 0,
-  //     LEFT: 0,
-  //   },
+  {
+    //tiles25
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tiles26
+    UP: 3,
+    RIGHT: 0,
+    DOWN: 3,
+    LEFT: 0,
+  },
+  {
+    //tiles27
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles28
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles29
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tiles30
+    UP: 3,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
   {
     //tiles31
     UP: 0,
@@ -236,8 +236,219 @@ const reglas = [
     DOWN: 0,
     LEFT: 0,
   },
+  {
+    //tiles32
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 3,
+    LEFT: 0,
+  },
+  {
+    //tiles33
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles34
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tiles35
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles36
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //tiles37
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 3,
+    LEFT: 2,
+  },
+  {
+    //tiles38
+    UP: 3,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles39
+    UP: 3,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tiles40
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 1,
+    LEFT: 2,
+  },
+  {
+    //tiles41
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 2,
+  },
+  {
+    //tiles42
+    UP: 3,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles43
+    UP: 0,
+    RIGHT: 2,
+    DOWN: 4,
+    LEFT: 2,
+  },
+  {
+    //tiles44
+    UP: 1,
+    RIGHT: 2,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //tiles45
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 2,
+  },
+  {
+    //tiles46
+    UP: 1,
+    RIGHT: 2,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tiles47
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 2,
+  },
+  {
+    //tiles48
+    UP: 4,
+    RIGHT: 0,
+    DOWN: 3,
+    LEFT: 0,
+  },
+  {
+    //tiles49
+    UP: 4,
+    RIGHT: 2,
+    DOWN: 3,
+    LEFT: 0,
+  },
+  {
+    //tiles50
+    UP: 4,
+    RIGHT: 0,
+    DOWN: 3,
+    LEFT: 2,
+  },
+  {
+    //tiles51
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 3,
+    LEFT: 0,
+  },
+  // {
+  //   //tiles52
+  //   UP: 1,
+  //   RIGHT: 2,
+  //   DOWN: 1,
+  //   LEFT: 0,
+  // },
+  // {
+  //   //tiles53
+  //   UP: 1,
+  //   RIGHT: 0,
+  //   DOWN: 1,
+  //   LEFT: 5,
+  // },
+  // {
+  //   //tiles54
+  //   UP: 1,
+  //   RIGHT: 5,
+  //   DOWN: 0,
+  //   LEFT: 0,
+  // },
+  // {
+  //   //tiles55
+  //   UP: 1,
+  //   RIGHT: 0,
+  //   DOWN: 0,
+  //   LEFT: 5,
+  // },
+  // {
+  //   //tiles56
+  //   UP: 3,
+  //   RIGHT: 0,
+  //   DOWN: 4,
+  //   LEFT: 0,
+  // },
+  // {
+  //   //tiles57
+  //   UP: 3,
+  //   RIGHT: 2,
+  //   DOWN: 4,
+  //   LEFT: 0,
+  // },
+  // {
+  //   //tiles58
+  //   UP: 3,
+  //   RIGHT: 0,
+  //   DOWN: 4,
+  //   LEFT: 5,
+  // },
+  // {
+  //   //tiles59
+  //   UP: 0,
+  //   RIGHT: 0,
+  //   DOWN: 4,
+  //   LEFT: 0,
+  // },
+  // {
+  //   //tiles60
+  //   UP: 0,
+  //   RIGHT: 5,
+  //   DOWN: 3,
+  //   LEFT: 0,
+  // },
+  // {
+  //   //tiles61
+  //   UP: 0,
+  //   RIGHT: 0,
+  //   DOWN: 3,
+  //   LEFT: 2,
+  // },
 ];
 //usamos estructura de datos con los object literals {} para darles un nombre a cada uno de los lados
+const NA = reglas.length; //numero de azulejos
 
 function preload() {
   for (let i = 0; i < NA; i++) {
@@ -248,7 +459,7 @@ function preload() {
 function setup() {
   createCanvas(1080, 1080);
 
-  frameRate(4);
+  // frameRate(4);
 
   ancho = width / RETICULA;
   alto = height / RETICULA;
@@ -263,9 +474,15 @@ function setup() {
     };
   }
 }
+
 function draw() {
-  background(225);
-  const celdasDisponibles = celdas.filter((celda) => {
+  // background(225);
+
+  const celdasConOpciones = celdas.filter((celda) => {
+    return celda.opciones.length > 0;
+  });
+
+  const celdasDisponibles = celdasConOpciones.filter((celda) => {
     return celda.colapsada == false;
   });
   if (celdasDisponibles.length > 0) {
@@ -280,6 +497,8 @@ function draw() {
     celdaSeleccionada.colapsada = true;
     const opcionSeleccionada = random(celdaSeleccionada.opciones);
     celdaSeleccionada.opciones = [opcionSeleccionada];
+
+    print(celdaSeleccionada);
 
     for (let x = 0; x < RETICULA; x++) {
       for (let y = 0; y < RETICULA; y++) {
@@ -321,17 +540,20 @@ function draw() {
               cambiarEntropia(celdaLEFT, reglasActuales["LEFT"], "RIGHT");
             }
           }
+        } else {
+          // strokeWeight(6);
+          // rect(x * ancho, y * alto, ancho, alto);
         }
       }
     }
     //noLoop
   } else {
-    for (let i = 0; i < RETICULA * RETICULA; i++) {
-      celdas[i] = {
-        colapsada: false,
-        opciones: opcionesI,
-      };
-    }
+    // for (let i = 0; i < RETICULA * RETICULA; i++) {
+    //   celdas[i] = {
+    //     colapsada: false,
+    //     opciones: opcionesI,
+    //   };
+    // }
   }
 }
 function cambiarEntropia(_celda, _regla, _opuesta) {
